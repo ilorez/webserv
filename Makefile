db64ab96f135d95d8ba4795a8b2b4725e8820c08
ilorez <zobirnajdaoui@gmail.com>
2026-02-19 16:09:55 +0000

    pre test

---
+++ Makefile
@@ -0,0 +1,23 @@
+CXX = g++
+CXXFLAGS = -Wall -Wextra -Werror -g
+SRC = main.cpp
+OBJ = $(SRC:.cpp=.o)
+EXEC = program
+
+all: $(EXEC)
+
+$(EXEC): $(OBJ)
+	$(CXX) $(OBJ) -o $(EXEC)
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(EXEC)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 14:48:30 by znajdaou          #+#    #+#             */
+/*   Updated: 2026/02/19 15:32:20 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h>   // socket(), bind(), listen(), accept()
+#include <netinet/in.h>   // struct sockaddr_in, htons(), INADDR_ANY
+#include <unistd.h>       // read(), write(), close()
+#include <arpa/inet.h>    // inet_addr() — optional for now
+//
+#define MAX_CONX_QUEUE 10
+#define BUF_SIZE 1024
+
+int main()
+{
+  int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (fd < 0)
+  {
+    std::cout << "Error with socket" << std::endl;
+    return (1);
+  }
+
+  int opt = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(8080);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  if (bind(fd, (struct sockaddr*)(&addr), sizeof(addr)))
+  {
+    std::cout << "Error with bind" << std::endl;
+    return 2;
+  }
+
+  if (listen(fd, MAX_CONX_QUEUE))
+  {
+    std::cout << "Error with listen" << std::endl;
+    return (3);
+  }
+
+  socklen_t size_socket = sizeof(addr);
+  while (1)
+  {
+    int accpetFd = accept(fd, (struct sockaddr*)(&addr), (socklen_t *)&size_socket);
+    if (accpetFd < 0)
+    {
+      std::cout << "Error with accept" << std::endl;
+      return (4);
+    }
+    char buf[1024]; 
+    memset(&buf, 0, BUF_SIZE);
+    // read from user fd
+    size_t readed = read(accpetFd, buf, BUF_SIZE);
+
+    // write back to user fd
+    write(accpetFd, buf, readed);
+
+    // close 
+    close(accpetFd);
+  }
+  close(fd);
+}
