764bb97ec599d3f69e7e568c3c04c51c26112b9b
ilorez <zobirnajdaoui@gmail.com>
2026-02-19 21:14:54 +0000

    return valid html header with simple message Hello World

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:48:30 by znajdaou          #+#    #+#             */
-/*   Updated: 2026/02/19 15:32:20 by znajdaou         ###   ########.fr       */
+/*   Updated: 2026/02/19 21:14:05 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <cstring>
@@ -60,10 +60,15 @@ int main()
     memset(&buf, 0, BUF_SIZE);
     // read from user fd
     size_t readed = read(accpetFd, buf, BUF_SIZE);
-
+    (void)readed;
+    std::string body = "<h1>Hello World</h1>";
+    std::string response = "HTTP/1.1 200 OK\r\n"
+                       "Content-Type: text/html\r\n"
+                       "Content-Length: " + std::to_string(body.length()) + "\r\n"
+                       "\r\n"
+                       + body;
     // write back to user fd
-    write(accpetFd, buf, readed);
-
+    write(accpetFd, response.c_str(), response.length());
     // close 
     close(accpetFd);
   }
