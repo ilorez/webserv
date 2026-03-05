1d7bb0df48a01221ff7c0ed1edebfe68478cd8a1
ali-ezghari <aezghari90@gmail.com>
2026-03-05 15:22:14 +0000

    request first line parsing

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utils.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/22 14:41:45 by znajdaou          #+#    #+#             */
-/*   Updated: 2026/02/23 00:14:54 by znajdaou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -32,22 +20,22 @@ std::string ft_readFile(std::string src)
 }
 
 
-size_t split(const std::string &txt, std::vector<std::string> &strs, char ch)
+size_t split(const std::string &txt, std::vector<std::string> &strs, const std::string del)
 {
-    size_t pos = txt.find( ch );
+    size_t pos = txt.find( del );
     size_t initialPos = 0;
     strs.clear();
 
     // Decompose statement
     while( pos != std::string::npos ) {
         strs.push_back( txt.substr( initialPos, pos - initialPos ) );
-        initialPos = pos + 1;
+        initialPos = pos + del.length();
 
-        pos = txt.find( ch, initialPos );
+        pos = txt.find( del, initialPos );
     }
 
     // Add the last one
-    strs.push_back( txt.substr( initialPos, std::min( pos, txt.size() ) - initialPos + 1 ) );
+    strs.push_back( txt.substr( initialPos ));
     return strs.size();
 }
 
